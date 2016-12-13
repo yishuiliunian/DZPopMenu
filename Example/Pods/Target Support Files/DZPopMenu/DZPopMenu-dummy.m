eb3282525c603f81e0673a855656eb923a03166d
yishuiliunian <yishuiliunian@gmail.com>
2016-12-13 12:26:59 +0800

    init

---
+++ Example/Pods/Target Support Files/DZPopMenu/DZPopMenu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DZPopMenu : NSObject
+@end
+@implementation PodsDummy_DZPopMenu
+@end
---
+++ Example/Pods/Target Support Files/Pods-DZPopMenu_Tests/Pods-DZPopMenu_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DZPopMenu_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DZPopMenu_Tests
+@end
